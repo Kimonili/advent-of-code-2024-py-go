a94273915e0643461b76f5797fa6526d7e427377
Kimon Iliopoulos <kimoniliopoulos@Kimons-MacBook-Air.local>
2024-12-02 16:37:05 +0000

    go day 1 part 2

---
+++ go/day-1/part-2/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"day1/utils"
+	"fmt"
+)
+
+func main() {
+	list1, list2 := utils.ReadInputFile("../input")
+
+	// Initialise a slice of ints with the same length as the first list
+	similarityList := make([]int, len(list1))
+
+	// Iterate over the second list
+	for i, num1 := range list1 {
+
+		simScore := utils.CalculateTimesInList(list2, num1)
+
+		similarityList[i] = simScore
+	}
+
+	// Calculate sum of similarity scores
+	sum := 0
+	for _, score := range similarityList {
+		sum += score
+	}
+
+	fmt.Printf("total similarity score: %d\n", sum)
+}
